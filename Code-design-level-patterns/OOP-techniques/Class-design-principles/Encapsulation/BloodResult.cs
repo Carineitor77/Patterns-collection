999a978c52f51a21bb9a2236104932c5ac3ffe12
Anatoly <90443893+Carineitor77@users.noreply.github.com>
2023-05-01 23:52:04 +0300

    Added-encapsulation

---
+++ Code-design-level-patterns/OOP-techniques/Class-design-principles/Encapsulation/BloodResult.cs
@@ -0,0 +1,20 @@
+﻿namespace Encapsulation;
+
+public class BloodResult
+{
+    public BloodResult(double leukocytes, double platelets, double erythrocytes)
+    {
+        Leukocytes = leukocytes;
+        Platelets = platelets;
+        Erythrocytes = erythrocytes;
+    }
+    
+    public double Leukocytes { get; }
+    public double Platelets { get; }
+    public double Erythrocytes { get; }
+
+    public bool IsNormal()
+        => Leukocytes is > 10 and < 100 
+           && Platelets is > 25 and < 120 
+           && Erythrocytes is > 15 and < 105;
+}
---
+++ Code-design-level-patterns/OOP-techniques/Class-design-principles/Encapsulation/Program.cs
@@ -0,0 +1,4 @@
+﻿using Encapsulation;
+
+var res = new BloodResult(45, 60, 75);
+Console.WriteLine(res.IsNormal());
